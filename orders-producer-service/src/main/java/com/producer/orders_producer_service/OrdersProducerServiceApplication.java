c4863c93df3849d53eedc2306e59c3e6e4c42620
SalindaGunarathna <salinda.eng@gmail.com>
2025-11-21 15:07:19 +0530

    configure avro

--- orders-producer-service/src/main/java/com/producer/orders_producer_service/OrdersProducerServiceApplication.java
+++ orders-producer-service/src/main/java/com/producer/orders_producer_service/OrdersProducerServiceApplication.java
@@ -2,8 +2,9 @@ package com.producer.orders_producer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class OrdersProducerServiceApplication {
 
 	public static void main(String[] args) {
