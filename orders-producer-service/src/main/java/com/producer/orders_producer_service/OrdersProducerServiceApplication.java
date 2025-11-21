a4f73cec2c545ef5ebe7390238cb6f0eb5053673
SalindaGunarathna <salinda.eng@gmail.com>
2025-11-21 10:22:38 +0530

    init project

---
+++ orders-producer-service/src/main/java/com/producer/orders_producer_service/OrdersProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.producer.orders_producer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersProducerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersProducerServiceApplication.class, args);
+	}
+
+}
